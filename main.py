25e4644340eebe74ee4d41b01908957042404cbf
Chaid <vida.mkp@obmontessori.edu.ph>
2025-09-20 12:54:11 +0800

    Add files via upload

---
+++ main.py
@@ -0,0 +1,26 @@
+from pyscript import document
+
+def click(event):
+    output_div = document.getElementById("output")
+    output_div.innerText = ""
+
+    name = document.getElementById("name").value
+    age = document.getElementById("age").value
+    school = document.getElementById("school").value
+
+    message = f"""
+    \nStudent Profile:
+    
+    Name: {name}
+    Age: {age}
+    School: {school}
+
+    \nNotes:
+    
+   Hello {name}!
+   You're currently {age} years old.
+   You go to {school}!
+   This website was made with HTML and Python Script.
+    """
+
+    output_div.innerText = message
